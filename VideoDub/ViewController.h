e526fa1bc649ee8bc1b9b5fd4d25d70aa117d7dd
RoseMax <erica.correa831@gmail.com>
2016-06-17 11:40:59 -0400

    Initial commit

---
+++ VideoDub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoDub
+//
+//  Created by Aditya Narayan on 6/17/16.
+//  Copyright © 2016 TurnToTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
